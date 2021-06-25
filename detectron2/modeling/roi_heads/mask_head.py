238c10161e3c60fc872ad1554acf3d7a71a1a0e2
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 17:48:01 +0530

    Update mask_head.py

--- detectron2/modeling/roi_heads/mask_head.py
+++ detectron2/modeling/roi_heads/mask_head.py
@@ -5,11 +5,19 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from detectron2.config import configurable
-from detectron2.layers import Conv2d, ConvTranspose2d, ShapeSpec, cat, get_norm
-from detectron2.structures import Instances
-from detectron2.utils.events import get_event_storage
-from detectron2.utils.registry import Registry
+# from detectron2.config import configurable
+# from detectron2.layers import Conv2d, ConvTranspose2d, ShapeSpec, cat, get_norm
+# from detectron2.structures import Instances
+# from detectron2.utils.events import get_event_storage
+# from detectron2.utils.registry import Registry
+
+
+
+from ...config import configurable
+from ...layers import Conv2d, ConvTranspose2d, ShapeSpec, cat, get_norm
+from ...structures import Instances
+from ...utils.events import get_event_storage
+from ...utils.registry import Registry
 
 __all__ = [
     "BaseMaskRCNNHead",
