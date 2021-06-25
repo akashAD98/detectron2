081305e5837c119d23a55cbc865ef11e91da1360
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 14:40:49 +0530

    Update __init__.py

--- detectron2/utils/__init__.py
+++ detectron2/utils/__init__.py
@@ -1,1 +1,14 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+def make_divisible(v, divisor=8, min_value=None):
+    """
+    The channel number of each layer should be divisable by 8.
+    The function is taken from
+    github.com/rwightman/pytorch-image-models/master/timm/models/layers/helpers.py
+    """
+    min_value = min_value or divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+© 2021 GitHub, Inc.
