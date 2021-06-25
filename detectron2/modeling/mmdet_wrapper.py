c826f9f7bde625e0c7ac6603881b8cefcab1c644
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 17:56:57 +0530

    Update mmdet_wrapper.py

--- detectron2/modeling/mmdet_wrapper.py
+++ detectron2/modeling/mmdet_wrapper.py
@@ -10,9 +10,9 @@ import torch
 from omegaconf import DictConfig, OmegaConf
 from torch import Tensor, nn
 
-from detectron2.layers import ShapeSpec
-from detectron2.structures import BitMasks, Boxes, ImageList, Instances
-from detectron2.utils.events import get_event_storage
+from ..layers import ShapeSpec
+from ..structures import BitMasks, Boxes, ImageList, Instances
+from ..utils.events import get_event_storage
 
 from .backbone import Backbone
 
