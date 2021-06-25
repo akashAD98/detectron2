25ee5ddba3d8968bb465d2ad6e51bb576aa515a6
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 16:18:55 +0530

    Update config.py

--- detectron2/config/config.py
+++ detectron2/config/config.py
@@ -6,7 +6,8 @@ import inspect
 import logging
 from fvcore.common.config import CfgNode as _CfgNode
 
-from detectron2.utils.file_io import PathManager
+#from detectron2.utils.file_io import PathManager
+from ..utils.file_io import PathManager
 
 
 class CfgNode(_CfgNode):
