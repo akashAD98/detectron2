3829c64057bbfbadc23b57d52d36073c833b38f8
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 17:55:25 +0530

    Update sampling.py

--- detectron2/modeling/sampling.py
+++ detectron2/modeling/sampling.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 import torch
 
-from detectron2.layers import nonzero_tuple
+from ..layers import nonzero_tuple
 
 __all__ = ["subsample_labels"]
 
