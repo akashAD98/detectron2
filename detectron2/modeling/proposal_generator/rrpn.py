0c30526dee4d242e2a6e2c9365fcd03bd7b8e3c0
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 17:44:23 +0530

    Update rrpn.py

--- detectron2/modeling/proposal_generator/rrpn.py
+++ detectron2/modeling/proposal_generator/rrpn.py
@@ -4,10 +4,17 @@ import logging
 from typing import Dict, List
 import torch
 
-from detectron2.config import configurable
-from detectron2.layers import ShapeSpec, batched_nms_rotated, cat
-from detectron2.structures import Instances, RotatedBoxes, pairwise_iou_rotated
-from detectron2.utils.memory import retry_if_cuda_oom
+# from detectron2.config import configurable
+# from detectron2.layers import ShapeSpec, batched_nms_rotated, cat
+# from detectron2.structures import Instances, RotatedBoxes, pairwise_iou_rotated
+# from detectron2.utils.memory import retry_if_cuda_oom
+
+
+from ...config import configurable
+from ...layers import ShapeSpec, batched_nms_rotated, cat
+from ...structures import Instances, RotatedBoxes, pairwise_iou_rotated
+from ...utils.memory import retry_if_cuda_oom
+
 
 from ..box_regression import Box2BoxTransformRotated
 from .build import PROPOSAL_GENERATOR_REGISTRY
