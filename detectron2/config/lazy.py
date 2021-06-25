685e197e68741c5a4f74b5252a6f6510f69d0e2b
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 16:20:43 +0530

    Update lazy.py

--- detectron2/config/lazy.py
+++ detectron2/config/lazy.py
@@ -14,8 +14,13 @@ import cloudpickle
 import yaml
 from omegaconf import DictConfig, ListConfig, OmegaConf
 
-from detectron2.utils.file_io import PathManager
-from detectron2.utils.registry import _convert_target_to_string
+#from detectron2.utils.file_io import PathManager
+#from detectron2.utils.registry import _convert_target_to_string
+
+
+
+from ..utils.file_io import PathManager
+from ..utils.registry import _convert_target_to_string
 
 __all__ = ["LazyCall", "LazyConfig"]
 
