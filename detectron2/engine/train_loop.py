d728c7d7bdce1215e79c85406934ce7d0ffdaa97
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2021-06-25 16:42:53 +0530

    Update train_loop.py

--- detectron2/engine/train_loop.py
+++ detectron2/engine/train_loop.py
@@ -9,9 +9,16 @@ from typing import Dict, List, Optional
 import torch
 from torch.nn.parallel import DataParallel, DistributedDataParallel
 
-import detectron2.utils.comm as comm
-from detectron2.utils.events import EventStorage, get_event_storage
-from detectron2.utils.logger import _log_api_usage
+# import detectron2.utils.comm as comm
+# from detectron2.utils.events import EventStorage, get_event_storage
+# from detectron2.utils.logger import _log_api_usage
+
+
+
+from ..utils import comm
+from ..utils.events import EventStorage, get_event_storage
+from ..utils.logger import _log_api_usage
+
 
 __all__ = ["HookBase", "TrainerBase", "SimpleTrainer", "AMPTrainer"]
 
